2e46b8cbc9d02997f94b2a68d7c4b2f89afd8f1e
fjhanevo <fredrik.hanevold@gmail.com>
2024-04-26 18:54:30 +0200

    removed plot from file fn

--- Assignment1/plotter.py
+++ Assignment1/plotter.py
@@ -1,7 +1,9 @@
+import numpy as np
 import matplotlib.pyplot as plt
+from crank_nicolson import crank_nicolson
 
-def plot(U,T,Xgrid,Tgrid):
-
+def plot_c_and_n(U,T,Xgrid,Tgrid):
+    
     time_interval = [0.1,0.2,0.3,0.4,0.5]
     plt.figure(figsize=(12,8))
     for t in time_interval:
