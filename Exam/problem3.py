c5398e0d676f3e9bd11d897cd86da1b268db8bed
fjhanevo <fredrik.hanevold@gmail.com>
2024-04-30 18:18:59 +0200

    fixed crank-nicolson scheme to work correctly

--- Exam/problem3.py
+++ Exam/problem3.py
@@ -67,39 +67,87 @@ def implicit_euler(V0,dx,dt,Nx,Nt):
         V_time[:,i] = V0
     return V_time
 
+def get_cn_matrices(Nx,dx,dt):
+    """
+    Initializes the A and B matrix used for the Crank-Nicolson scheme. 
+    Applies Neumann boundary conditions
+    """
+    diag = np.ones((Nx))
+    alpha = dt/(2*dx**2)
+
+    A = (-alpha/2) * np.diag(diag[1:], -1) +\
+        (-alpha/2) * np.diag(diag[1:],1) +\
+        (1+alpha)*np.diag(diag)
+
+    B = (alpha/2) * np.diag(diag[1:], -1) +\
+        (alpha/2) * np.diag(diag[1:],1) +\
+        (1-alpha) * np.diag(diag)
+    
+    A[0,0] = 1 + alpha/2
+    A[-1,-1] = 1 + alpha/2
+    B[0,0] = 1 - alpha/2
+    B[-1,-1] = 1 - alpha/2
+
+    return A,B
+
 def crank_nicolson(V0,dx,dt,Nx,Nt):
     """ Crank-Nicolson scheme for the cable equation """
-    alpha = 1*dt/(dx**2)    # CFL number, D = 1
-
-    # Setup matrix for spatial discretization with Neumann bc's
-    diag = np.ones(Nx) * (1+2*alpha)
-    off_diag = np.ones(Nx-1) * (-alpha)
-    A =  diags([diag,off_diag,off_diag], [0,-1,1],format='csr')
-    A[0,1] = -2 * alpha
-    A[-1,-2] = -2*alpha
-
-    # Matrices for CN-scheme
-    I = identity(Nx,format='csr')
-    CN_lhs = I-0.5*dt*A-0.5*dt*I
-    CN_rhs = I+0.5*dt*A+0.5*dt*I
-
-    # Matrix to store results, set inital condition
+ 
+    # Initialize V_time to store results 
     V_time = np.zeros((Nx,Nt))
+
+    # Apply initial condition
     V_time[:,0] = V0
 
-    # Time stepping
-    for i in range(1,Nt):
-        b = CN_rhs @ V0
-        V0 = spsolve(CN_lhs,b)
-        V_time[:,i] = V0
+    # Get matrices
+    A, B = get_cn_matrices(Nx,dx,dt)
+
+    # Solve the equation
+    for i in range(Nt-1):
+        u = B@V_time[:,i]
+        V_time[:,i+1] = np.linalg.solve(A,u)
     
-    return V_time
+    return V_time 
 
 def analytical_solution(V0,x,t,x0):
     """ Implementation of the analytical solution """
     # lamda = tau = 1.0
     return (V0/np.sqrt(4*np.pi*t))*(np.exp(-((x-x0)**2)/(4*t-t)))
 
+def plot_method(V,X,time_steps,title,filename=None):
+    """ Plots the time evolution of a single method"""
+    plt.figure(figsize=(10,6))
+    for t in time_steps:
+        index = int(t*(Nt-1))
+        plt.plot(X,V[:,index],label=f't={t=:.2f}s')
+    plt.title(title)
+    plt.xlabel('Position [m]')
+    plt.ylabel('Voltage [V]')
+    plt.legend()
+    if filename is not None:
+        plt.savefig(filename)
+    else:
+        plt.tight_layout()
+        plt.show()
+
+def plot_analytical(X,time_steps, filename=None):
+    """ Plots only the analytical solution """
+    for t in time_steps:
+        V = analytical_solution(V_t,X,t,x0)
+        plt.plot(X,V,label=f't={t:.2f}s')
+    plt.title('Analytical solution')
+    plt.xlabel('Position [m]')
+    plt.ylabel('Voltage [V]')
+    plt.legend()
+    if filename is not None:
+        plt.savefig(filename)
+    else:
+        plt.tight_layout()
+        plt.show()
+
+
+
+""" Keeping this here for comparison"""
 def plot_methods(X,V_explicit,V_implicit,V_cn,time_steps):
     """ Plots the method in a 2x2 square for easy comparison """
     # fig, axs = plt.subplots(2,2,figsize=(12,12),sharex=True,sharey=True)
@@ -136,16 +184,23 @@ def plot_methods(X,V_explicit,V_implicit,V_cn,time_steps):
 
     plt.tight_layout()
     plt.show()
+    fig.savefig('test.png')
 
-
-V_explicit = explicit_euler(V0,dx,dt,Nx,Nt)
+# dx1 = 0.01
+dt_explicit = dx**2/Nx*10
+# alpha = dt_explicit/(2*dx**2)
+# print(alpha)
+V_explicit = explicit_euler(V0,dx,dt_explicit,Nx,Nt)
 V_implicit = implicit_euler(V0,dx,dt,Nx,Nt) 
 V_crank_nicolson = crank_nicolson(V0,dx,dt,Nx,Nt)
 V_t = 0.2
 # V_analytical = analytical_solution(V_t,X,T,x0)
-time_steps = [T[1],T[25],T[50],T[800]]
+time_steps = [T[1],T[25],T[50],T[450],T[900]]
 # time_steps = [int(Nt/4),int(Nt/2),int(3*Nt/4),Nt-1]
 # time_steps = [T[int(Nt/4)],T[int(Nt/2)],T[int(3*Nt/4)],T[Nt-1]]
 # time_steps = [T[int(Nt/8)],T[int(Nt/4)],T[int(Nt/2)],T[Nt-1]]
+# plot_method(V_explicit,X,time_steps,'Explicit Euler')
+# plot_analytical(X,time_steps,'Figures/analytical')
 
 plot_methods(X,V_explicit,V_implicit,V_crank_nicolson,time_steps)
+
